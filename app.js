924cc872cd44a6f3d32ceb5d27077bea5a6b69bb
Dylan Palmboom <nofxboy1234@gmail.com>
2025-05-18 22:04:51 +0200

    Extract route groups to their own modules

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 import "dotenv/config";
 import express from "express";
+import authorRouter from "./routes/authorRouter.js";
+import bookRouter from "./routes/bookRouter.js";
+import indexRouter from "./routes/indexRouter.js";
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello, world!"));
+app.use("/authors", authorRouter);
+app.use("/books", bookRouter);
+app.use("/", indexRouter);
+
+// app.get("/", (req, res) => res.send("Hello, world!"));
 
 const PORT = process.env.PORT;
 
