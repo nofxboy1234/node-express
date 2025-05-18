68c5e004cdcac1e6c9c5eee8303eb4025de239b8
Dylan Palmboom <nofxboy1234@gmail.com>
2025-05-18 17:12:29 +0200

    Install dotenv and use for PORT

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+import "dotenv/config";
 import express from "express";
 
 const app = express();
 
 app.get("/", (req, res) => res.send("Hello, world!"));
 
-const PORT = 3000;
+const PORT = process.env.PORT;
+
 app.listen(PORT, () => {
   console.log(`My first Express app - listening on port ${PORT}`);
 });
