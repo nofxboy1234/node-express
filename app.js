2e806e57cd771450123aa6d8a778f3300bb5f9c7
Dylan Palmboom <nofxboy1234@gmail.com>
2025-05-19 14:12:25 +0200

    Fix __dirname

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ import authorRouter from "./routes/authorRouter.js";
 import bookRouter from "./routes/bookRouter.js";
 import indexRouter from "./routes/indexRouter.js";
 import path from "node:path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
