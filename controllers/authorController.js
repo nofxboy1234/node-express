96d1798383c99350f884a10451941d6a133aa02c
Dylan Palmboom <nofxboy1234@gmail.com>
2025-05-19 09:58:23 +0200

    Add authorController

---
+++ controllers/authorController.js
@@ -0,0 +1,16 @@
+import db from "../db.js";
+
+async function getAuthorById(req, res) {
+  const { authorId } = req.params;
+
+  const author = await db.getAuthorById(Number(authorId));
+
+  if (!author) {
+    res.status(404).send("Author not found");
+    return;
+  }
+
+  res.send(`Author Name: ${author.name}`);
+}
+
+export { getAuthorById };
--- routes/authorRouter.js
+++ routes/authorRouter.js
@@ -1,11 +1,9 @@
 import { Router } from "express";
+import { getAuthorById } from "../controllers/authorController.js";
 
 const authorRouter = Router();
 
 authorRouter.get("/", (req, res) => res.send("All authors"));
-authorRouter.get("/:authorId", (req, res) => {
-  const { authorId } = req.params;
-  res.send(`Author ID: ${authorId}`);
-});
+authorRouter.get("/:authorId", getAuthorById);
 
 export default authorRouter;
