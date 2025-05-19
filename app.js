d63577e64650b07463e57943bc69388b22bcf3f3
Dylan Palmboom <nofxboy1234@gmail.com>
2025-05-19 10:33:34 +0200

    Add error middleware

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ app.use("/authors", authorRouter);
 app.use("/books", bookRouter);
 app.use("/", indexRouter);
 
+// app.use((req, res, next) => {
+//   throw new Error("OH NO!");
+// });
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send(err.message);
+});
+
 // app.get("/", (req, res) => res.send("Hello, world!"));
 
 const PORT = process.env.PORT;
