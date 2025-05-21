73dcdbd252482dd2da0b8dcd5d9ca4cf9e62d20c
Dylan Palmboom <nofxboy1234@gmail.com>
2025-05-21 10:46:59 +0200

    User bcryptjs for securing passwords

--- auth/passport.js
+++ auth/passport.js
@@ -1,6 +1,7 @@
 import passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
 import db from "../db/node_auth_basics/queries.js";
+import bcrypt from "bcryptjs";
 
 passport.use(
   new LocalStrategy(async (username, password, done) => {
@@ -10,7 +11,8 @@ passport.use(
       if (!user) {
         return done(null, false, { message: "Incorrect username" });
       }
-      if (user.password !== password) {
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
         return done(null, false, { message: "Incorrect password" });
       }
       return done(null, user);
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 import db from "../db/node_auth_basics/queries.js";
+import bcrypt from "bcryptjs";
 
 async function index(req, res) {
   console.log("*** req.user: ", req.user);
@@ -12,7 +13,8 @@ async function newUser(req, res) {
 async function create(req, res, next) {
   try {
     const { username, password } = req.body;
-    await db.insertAuthUser(username, password);
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await db.insertAuthUser(username, hashedPassword);
 
     const user = await db.findAuthUserByUsername(username);
     req.login(user, () => {
